31791289173ad33f73efed07fd207a8be81d2b15
David Suh <davidisuh93@gmail.com>
2021-01-25 19:04:13 -0500

    tried debugging back button issue

--- frontend/components/beer/beer_index_item.jsx
+++ frontend/components/beer/beer_index_item.jsx
@@ -18,27 +18,33 @@ class BeerIndexItem extends React.Component {
     const yearDigits = dateFormat[0].split('');
     const yearFormat = [yearDigits[2], yearDigits[3]].join('');
     const newDateFormat = [dateFormat[1], dateFormat[2], yearFormat].join('/');
-    return (
-      <div className="beer-item">
-        <div className="beer-item-top">
-          <img className="beer-item-img" src={window.beerPic} alt=""/>
-          <div className="beer-item-details">
-            <Link id="beer-item-name" to={`/beers/${beer.id}`}>{beer.beer_name}</Link>
-            <Link id="beer-item-brewery" to={`/breweries/${beer.brewery.id}`}>{beer.brewery.brewery_name}</Link>
-            <p id="beer-item-style">{beer.style}</p>
-            <p id="beer-item-description">{beer.description}</p>
+    if (!this.props.beer) {
+      return (
+        <img className="loading" src={window.loader}/>
+      )
+    } else {
+      return (      
+        <div className="beer-item">
+          <div className="beer-item-top">
+            <img className="beer-item-img" src={window.beerPic} alt=""/>
+            <div className="beer-item-details">
+              <Link id="beer-item-name" to={`/beers/${this.props.beer.id}`}>{beer.beer_name}</Link>
+              <Link id="beer-item-brewery" to={`/breweries/${beer.brewery.id}`}>{beer.brewery.brewery_name}</Link>
+              <p id="beer-item-style">{beer.style}</p>
+              <p id="beer-item-description">{beer.description}</p>
+            </div>
+            <button className="beer-item-btn">checkin-button</button>
           </div>
-          <button className="beer-item-btn">checkin-button</button>
+          <div className="beer-item-bottom">
+            <div id="abv">{beer.abv}% ABV</div>
+            <div id="ibu">{beer.ibu ? beer.ibu : 'N/A'} IBU</div>
+            <div id="beer-rating-bar">rating bar</div>
+            <div id="beer-total-ratings">total Ratings</div>
+            <div id="beer-created-at">Added {newDateFormat}</div>
+          </div>
         </div>
-        <div className="beer-item-bottom">
-          <div id="abv">{beer.abv}% ABV</div>
-          <div id="ibu">{beer.ibu ? beer.ibu : 'N/A'} IBU</div>
-          <div id="beer-rating-bar">rating bar</div>
-          <div id="beer-total-ratings">total Ratings</div>
-          <div id="beer-created-at">Added {newDateFormat}</div>
-        </div>
-      </div>
-    )
+      )
+    }
   }
 }
 
