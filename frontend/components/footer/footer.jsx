48bf23c7502e3dcdc49ead383dfd0931b038bfe1
David Suh <davidisuh93@gmail.com>
2021-01-26 21:38:46 -0500

    fixed footer github logo

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => (
       <p>This project is one of the firstfruits of over 1,200 hours in an intense coding bootcamp at App Academy based in NYC.</p>
     </div>
     <div className="footer">
-      <a href="https://github.com/dsuh93"><img src="https://www.flaticon.com/svg/vstatic/svg/25/25231.svg?token=exp=1611691104~hmac=31102455d65cecaf184c6a6e6fe83fd3"/></a>
+      <a href="https://github.com/dsuh93"><img src={window.github}/></a>
       <a href=""><img src="https://untappd.akamaized.net/assets/custom/homepage/images/icon-facebook.svg"/></a>
       <a href=""><img src="https://untappd.akamaized.net/assets/custom/homepage/images/icon-instagram.svg"/></a>
     </div>
