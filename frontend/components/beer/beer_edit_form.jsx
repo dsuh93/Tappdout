d72f83b3bb46260d72b56dbbf6cf6820a5f38241
David Suh <davidisuh93@gmail.com>
2021-01-27 12:12:36 -0500

    finished fixing errors

--- frontend/components/beer/beer_edit_form.jsx
+++ frontend/components/beer/beer_edit_form.jsx
@@ -12,6 +12,10 @@ class BeerEditForm extends React.Component {
       .then( () => this.setState(this.props.beer))
   }
 
+  componentWillUnmount() {
+    this.props.receiveBeerErrors([])
+  }
+
   handleSubmit(e) {
     e.preventDefault();
     const beer = Object.assign({}, this.state);
@@ -33,6 +37,13 @@ class BeerEditForm extends React.Component {
 
 
   render() {
+    const errors = this.props.errors.map( error => {
+      return (
+        <li key={error}>{error}</li>
+      )
+    })
+    const errorsClassname = this.props.errors.length > 0 ? "beer-form-errors-reveal" : "hidden";
+
     const beer = this.state;
     if(!beer && !this.props.beer) {
       return (
@@ -42,94 +53,99 @@ class BeerEditForm extends React.Component {
       )
     } else {
       return (
-        <div className="beer-form-container">
-          <div className="beer-form-instructions">
-            <h1>Propose an Edit</h1>
-            <p>Did you notice something incorrect with the details of this beer? Help us out by proposing an edit. Once submitted, our team will review and make the change if necessary.</p>
-          </div>
-          <form  onSubmit={this.handleSubmit} className="beer-form">
-            <label>BEER NAME</label>
-              <input type="text"
-                value={beer ? beer.beer_name : this.props.beer.beer_name}
-                onChange={this.update("beer_name")}
-              />
-            <label>BREWERY NAME</label>
-            <input type="text"
-                value={beer ? beer.brewery.brewery_name : this.props.beer.brewery_name}
-                disabled
-              />
-            <div className="beer-form-3">
-              <div>
-                <label>ABV</label>
-                  <input type="number"
-                    value={beer ? beer.abv : this.props.beer.abv}
-                    onChange={this.update("abv")}
-                  />
-              </div>
-              <div>
-                <label>IBU</label>
-                <input type="number"
-                    value={beer ? beer.ibu : this.props.beer.ibu}
-                    onChange={this.update("ibu")}
-                  />
-              </div>
-              <div className="beer-form-style-div">
-                <label>STYLE</label>
-                  <select id="beer-form-style"
-                    type="text"
-                    value={beer ? beer.style : this.props.beer.style}
-                    onChange={this.update("style")}>
-                      <option value>Select a Style</option>
-                      <option value="American Wild Ale">American Wild Ale</option>
-                      <option value="Belgian Blonde">Belgian Blonde</option>
-                      <option value="Black and Tan">Black and Tan</option>
-                      <option value="Blonde Ale">Blonde Ale</option>
-                      <option value="Brown Ale">Brown Ale</option>
-                      <option value="Cider">Cider</option>
-                      <option value="Dark Ale">Dark Ale</option>
-                      <option value="Farmhouse Ale">Farmhouse Ale</option>
-                      <option value="Fruit Beer">Fruit Beer</option>
-                      <option value="German Ale">German Ale</option>
-                      <option value="Ginger Beer">Ginger Beer</option>
-                      <option value="Happoshu">Happoshu</option>
-                      <option value="Hard Seltzer">Hard Seltzer</option>
-                      <option value="Hefeweizen">Hefeweizen</option>
-                      <option value="IPA - American">IPA - American</option>
-                      <option value="IPA - Belgian">IPA - Belgian</option>
-                      <option value="IPA - Black">IPA - Black</option>
-                      <option value="IPA - English">IPA - English</option>
-                      <option value="IPA - Imperial">IPA - Imperial</option>
-                      <option value="IPA - Sour">IPA - Sour</option>
-                      <option value="IPA - White">IPA - White</option>
-                      <option value="Lager - American">Lager - American</option>
-                      <option value="Lager - Dark">Lager - Dark</option>
-                      <option value="Lager - Pale">Lager - Pale</option>
-                      <option value="Lager - Winter">Lager - Winter</option>
-                      <option value="Malt Beer">Malt Beer</option>
-                      <option value="Malt Liquor">Malt Liquor</option>
-                      <option value="Mead">Mead</option>
-                      <option value="Old Ale">Old Ale</option>
-                      <option value="Pale Ale">Pale Ale</option>
-                      <option value="Pilsner">Pilsner</option>
-                      <option value="Porter">Porter</option>
-                      <option value="Scotch Ale">Scotch Ale</option>
-                      <option value="Sour">Sour</option>
-                      <option value="Stout">Stout</option>
-                      <option value="Strong">Strong</option>
-                      <option value="Tradition Ale">Tradition Ale</option>
-                      <option value="Wheat Beer">Wheat Beer</option>
-                      <option value="Winter Ale">Winter Ale</option>
-                  </select>
-                  <div className="select-arrow-down"></div>
-              </div>
+        <div className="beer-form-page">
+          <div className="beer-form-container">
+            <div className="beer-form-instructions">
+              <h1>Propose an Edit</h1>
+              <p>Did you notice something incorrect with the details of this beer? Help us out by proposing an edit. Once submitted, our team will review and make the change if necessary.</p>
             </div>
-            <label>DESCRIPTION</label>
-              <textarea cols="30" rows="5"
-                value={beer ? beer.description : this.props.beer.description}
-                onChange={this.update("description")}
-              ></textarea>
-            <button className="beer-form-btn">Edit Beer</button>
-          </form>
+            <ul className={errorsClassname}>
+              {errors}
+            </ul>
+            <form  onSubmit={this.handleSubmit} className="beer-form">
+              <label>BEER NAME</label>
+                <input type="text"
+                  value={beer ? beer.beer_name : this.props.beer.beer_name}
+                  onChange={this.update("beer_name")}
+                />
+              <label>BREWERY NAME</label>
+              <input type="text"
+                  value={beer ? beer.brewery.brewery_name : this.props.beer.brewery_name}
+                  disabled
+                />
+              <div className="beer-form-3">
+                <div>
+                  <label>ABV</label>
+                    <input type="number"
+                      value={beer ? beer.abv : this.props.beer.abv}
+                      onChange={this.update("abv")}
+                    />
+                </div>
+                <div>
+                  <label>IBU</label>
+                  <input type="number"
+                      value={beer ? beer.ibu : this.props.beer.ibu}
+                      onChange={this.update("ibu")}
+                    />
+                </div>
+                <div className="beer-form-style-div">
+                  <label>STYLE</label>
+                    <select id="beer-form-style"
+                      type="text"
+                      value={beer ? beer.style : this.props.beer.style}
+                      onChange={this.update("style")}>
+                        <option value>Select a Style</option>
+                        <option value="American Wild Ale">American Wild Ale</option>
+                        <option value="Belgian Blonde">Belgian Blonde</option>
+                        <option value="Black and Tan">Black and Tan</option>
+                        <option value="Blonde Ale">Blonde Ale</option>
+                        <option value="Brown Ale">Brown Ale</option>
+                        <option value="Cider">Cider</option>
+                        <option value="Dark Ale">Dark Ale</option>
+                        <option value="Farmhouse Ale">Farmhouse Ale</option>
+                        <option value="Fruit Beer">Fruit Beer</option>
+                        <option value="German Ale">German Ale</option>
+                        <option value="Ginger Beer">Ginger Beer</option>
+                        <option value="Happoshu">Happoshu</option>
+                        <option value="Hard Seltzer">Hard Seltzer</option>
+                        <option value="Hefeweizen">Hefeweizen</option>
+                        <option value="IPA - American">IPA - American</option>
+                        <option value="IPA - Belgian">IPA - Belgian</option>
+                        <option value="IPA - Black">IPA - Black</option>
+                        <option value="IPA - English">IPA - English</option>
+                        <option value="IPA - Imperial">IPA - Imperial</option>
+                        <option value="IPA - Sour">IPA - Sour</option>
+                        <option value="IPA - White">IPA - White</option>
+                        <option value="Lager - American">Lager - American</option>
+                        <option value="Lager - Dark">Lager - Dark</option>
+                        <option value="Lager - Pale">Lager - Pale</option>
+                        <option value="Lager - Winter">Lager - Winter</option>
+                        <option value="Malt Beer">Malt Beer</option>
+                        <option value="Malt Liquor">Malt Liquor</option>
+                        <option value="Mead">Mead</option>
+                        <option value="Old Ale">Old Ale</option>
+                        <option value="Pale Ale">Pale Ale</option>
+                        <option value="Pilsner">Pilsner</option>
+                        <option value="Porter">Porter</option>
+                        <option value="Scotch Ale">Scotch Ale</option>
+                        <option value="Sour">Sour</option>
+                        <option value="Stout">Stout</option>
+                        <option value="Strong">Strong</option>
+                        <option value="Tradition Ale">Tradition Ale</option>
+                        <option value="Wheat Beer">Wheat Beer</option>
+                        <option value="Winter Ale">Winter Ale</option>
+                    </select>
+                    <div className="select-arrow-down"></div>
+                </div>
+              </div>
+              <label>DESCRIPTION</label>
+                <textarea cols="30" rows="5"
+                  value={beer ? beer.description : this.props.beer.description}
+                  onChange={this.update("description")}
+                ></textarea>
+              <button className="beer-form-btn">Edit Beer</button>
+            </form>
+          </div>
         </div>
       )
     }
--- frontend/components/beer/beer_edit_form_container.js
+++ frontend/components/beer/beer_edit_form_container.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { updateBeer, fetchBeer } from '../../actions/beer_actions';
+import { updateBeer, fetchBeer, receiveBeerErrors } from '../../actions/beer_actions';
 import BeerEditForm from './beer_edit_form';
 
 const msp = ( state, ownProps ) => {
   const beerId = ownProps.match.params.beerId;
   return ({
     beer: state.entities.beers[beerId],
-    beerId: beerId
+    beerId: beerId,
+    errors: state.errors.beerForm
   })
 };
 
 const mdp = dispatch => ({
   updateBeer: (beer) => dispatch(updateBeer(beer)),
-  fetchBeer: (beerId) => dispatch(fetchBeer(beerId))
+  fetchBeer: (beerId) => dispatch(fetchBeer(beerId)),
+  receiveBeerErrors: (error) => dispatch(receiveBeerErrors(error))
 });
 
 export default withRouter(connect(msp, mdp)(BeerEditForm));
