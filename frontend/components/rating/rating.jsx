095ba8ae0db7d992297b2b0eabad8a8644c5feba
David Suh <davidisuh93@gmail.com>
2021-01-28 15:30:17 -0500

    fixed rating bar view

--- frontend/components/rating/rating.jsx
+++ frontend/components/rating/rating.jsx
@@ -12,16 +12,16 @@ class Rating extends React.Component {
     for(let i = 1; i <= 5; i++) {
       if ( i <= r ) {
         imageArray.push( <img key={r + i} id="cap" src={window.capFull}/> )
-      } else if ( r < i ) {
+      } else if ( r < i && i === Math.ceil(r)) {
         if ( r % 1 === 0.25) {
           imageArray.push( <img key={r + i} id="cap" src={window.capFQ}/> )
         } else if ( r % 1 === 0.5) {
           imageArray.push( <img key={r + i} id="cap" src={window.capHalf}/> )
         } else if ( r % 1 === 0.75) {
           imageArray.push( <img key={r + i} id="cap" src={window.capLQ}/> )
-        } else {
-          imageArray.push( <img key={r + i} id="cap" src={window.capFull}/> )
-        }
+        } 
+      } else {
+        imageArray.push( <img key={r + i} id="cap" src={window.capEmpty}/> )
       }
     }
     return imageArray;
