faa122f0b594acd0f10a2578765fa03637a926f1
David Suh <davidisuh93@gmail.com>
2021-01-28 02:43:32 -0500

    finished AWS setup for modal and confirmed checkin form works

--- frontend/components/checkin/checkin_form.jsx
+++ frontend/components/checkin/checkin_form.jsx
@@ -12,9 +12,11 @@ class CheckinForm extends React.Component {
     };
     this.handleFile = this.handleFile.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    debugger
   }
 
   handleSubmit(e) {
+    debugger
     e.preventDefault();
     const formData = new FormData();
     formData.append('checkin[rating]', this.state.rating);
@@ -23,11 +25,31 @@ class CheckinForm extends React.Component {
     formData.append('checkin[user_id]', this.state.user_id);
     formData.append('checkin[photo]', this.state.photoFile);
     this.props.createCheckin(formData);
+    this.props.closeModal();
+    this.props.history.push('/home');
   }
 
   handleFile(e) {
+    debugger
     e.preventDefault();
-    this.setState({ photo: e.currentTarget.files[0]})
+    this.setState({ photoFile: e.currentTarget.files[0]})
+    this.showPhoto();
+  }
+
+  update(key) {
+    return (e) => {
+      e.preventDefault();
+      this.setState({ [key]: e.target.value })
+    }
+  }
+
+  showPhoto() {
+    const file = document.getElementById('file-upload').files[0];
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      document.getElementById('upload').src=(e.target.result);
+    }
+    reader.readAsDataURL(file);
   }
 
   render() {
@@ -38,13 +60,20 @@ class CheckinForm extends React.Component {
           <button onClick={() => this.props.closeModal()}><img src={window.modalClose}/></button>
         </div>
         <form className="checkin-form" onSubmit={this.handleSubmit}>
-          <textarea placeholder="What did you think?" className="checkin-form-textarea" cols="50" rows="5"></textarea>
-          <label htmlFor="file-upload" className="custom-file-upload"><img src={window.addPhoto}/></label>
-          <input id="file-upload" onChange={this.handleFile} type="file" className="add-photo-input"/>
-          {/* <img src={window.addPhoto} alt="add-photo-icon"/> */}
+          <textarea
+            placeholder="What did you think?"
+            className="checkin-form-textarea"
+            cols="50"
+            rows="5"
+            value={this.state.description}
+            onChange={this.update('description')}
+          ></textarea>
+          <label htmlFor="file-upload" className="photo-upload"><img id="upload" src={window.addPhoto}/></label>
+          <input id="file-upload" onChange={this.handleFile} type="file"/>
           <div className="checkin-form-bottom">
             <div className="ratings-bar-container">
               <div className="ratings-bar">Rating Bar</div>
+              <input onChange={this.update('rating')} type="number" step="0.25" min="0" max="5"/>
               <div className="ratings-value">NO RATING</div>
             </div>
             <button className="checkin-submit-btn">Confirm</button>
