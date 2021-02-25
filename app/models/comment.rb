f7ac1d786c5af761c1972b98ceedd71d18de1473
David Suh <davidisuh93@gmail.com>
2021-02-25 11:21:48 -0500

    add validations and associations for comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,11 @@
 class Comment < ApplicationRecord
+  validates :body, :author_id, :checkin_id, presence: true
+
+  belongs_to :author,
+    foreign_key: :author_id,
+    class_name: :User
+
+  belongs_to :checkin,
+    foreign_key: :checkin_id,
+    class_name: :Checkin 
 end
