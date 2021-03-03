408ccb237dfb646f983bb62a543ac1ed70507aad
David Suh <davidisuh93@gmail.com>
2021-03-03 00:37:56 -0500

    add profile container

---
+++ frontend/components/profile/profile_container.js
@@ -0,0 +1,13 @@
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Profiel from './profile';
+
+const msp = (state, ownProps) => ({
+
+})
+
+const mdp = dispatch => ({
+
+})
+
+export default withRouter(connect(msp, mdp)(Profile));
